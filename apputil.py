210a2ad7808211ec802ac75f9183142d6e1d9460
Bella Roberson <162742673+brobers0339@users.noreply.github.com>
2025-09-30 23:22:40 +0000

    Base of exervise 2 completed, still missing comments/docstring for code and need to add visualizations to answer questions

--- apputil.py
+++ apputil.py
@@ -1,8 +1,10 @@
 import plotly.express as px
 import pandas as pd
 
+df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
+
+
 def survival_demographics():
-    df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
     ages = [0, 12, 19, 59, float('inf')]
     labels = ['Child', 'Teen', 'Adult', 'Senior']
     df['age_group'] = pd.cut(df['Age'], bins=ages, labels=labels, right=True, include_lowest=True)
@@ -14,4 +16,19 @@ def survival_demographics():
     )
     df_grouped['survival_rate'] = df_grouped['n_survivors'] / df_grouped['n_passengers']
     return df_grouped
+
+def family_groups():
+    df['family_size'] = df['Parch'] + df['SibSp'] + 1
+    family_grouped = df.groupby(['Pclass', 'family_size']).agg(
+        n_passengers = ('PassengerId', 'count'),
+        avg_fare = ('Fare', 'mean'),
+        min_fare = ('Fare', 'min'),
+        max_fare = ('Fare', 'max')
+    )
+    return family_grouped
+
+def last_names():
+    last_names = df['Name'].str.split(',', expand=True)[0].str.strip()
+    return last_names.value_counts()
+
 

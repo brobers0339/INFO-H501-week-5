4c48d692402df9c66ff4c2bad3f68f57fca692fe
Bella Roberson <162742673+brobers0339@users.noreply.github.com>
2025-09-30 22:11:09 +0000

    Started exercise 1, still needs comments and final graph added! Pushing now to save progress

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ from apputil import *
 # Load Titanic dataset
 df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
 
+
 st.write(
 '''
 # Titanic Visualization 1
 
 '''
 )
+st.write("We've all heard the saying 'Women and Children first' from the retelling of the titanic. However, did the class of an individual affect the survival rate of women and children?")
 # Generate and display the figure
 fig1 = visualize_demographic()
 st.plotly_chart(fig1, use_container_width=True)
--- apputil.py
+++ apputil.py
@@ -1,4 +1,17 @@
 import plotly.express as px
 import pandas as pd
 
-# update/add code below ...
+def survival_demographics():
+    df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
+    ages = [0, 12, 19, 59, float('inf')]
+    labels = ['Child', 'Teen', 'Adult', 'Senior']
+    df['age_group'] = pd.cut(df['Age'], bins=ages, labels=labels, right=True, include_lowest=True)
+    df['age_group'] = df['age_group'].astype('category')
+
+    df_grouped = df.groupby(['Pclass', 'Sex', 'age_group']).agg(
+        n_passengers = ('PassengerId', 'count'),
+        n_survivors = ('Survived', 'sum')
+    )
+    df_grouped['survival_rate'] = df_grouped['n_survivors'] / df_grouped['n_passengers']
+    return df_grouped
+
